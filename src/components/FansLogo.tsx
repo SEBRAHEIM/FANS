f72fcb165137ae0af169f58f71124b72308e8a12
flightstrips.ATC <pymtydvxbz@privaterelay.appleid.com>
2026-02-09 16:10:34 +0400

    chore: bump logo cache version to v=3

--- src/components/FansLogo.tsx
+++ src/components/FansLogo.tsx
@@ -11,7 +11,7 @@ interface FansLogoProps {
 export default function FansLogo({ className = "h-8", variant = 'default' }: FansLogoProps) {
     return (
         <Image
-            src="/fans-logo.png?v=2"
+            src="/fans-logo.png?v=3"
             alt="FANS Logo"
             width={200}
             height={80}
